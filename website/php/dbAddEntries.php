676afc1b7bac10cc158a4e97c1574e29dbcf0f3f
robinvp <robinwp@hotmail.se>
2018-11-13 14:10:22 +0100

    Made a simple script to fill the database with redundant entries

---
+++ website/php/dbAddEntries.php
@@ -0,0 +1,57 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'admin';
+    $dbpass = 'adminpass';
+    $db = 'maindb';
+    
+    $dbconn=mysqli_connect($dbhost,$dbuser,$dbpass, $db);
+
+    if($dbconn->connect_error){
+        die("Database connection failed: " . $dbconn->connect_error);
+    }
+    
+    /*Below are entries added into the products table */
+    echo "connected.. \n";
+
+    $query = "INSERT INTO Products VALUES (NULL, 'Prada sko', 5000, 9, 99, 'x', 'xxx');";
+
+    mysqli_query($dbconn, $query);
+
+    echo "added new entry into database \n";
+
+    $query = "INSERT INTO Products VALUES (NULL, 'Raket', 1000000, 1, 101, 'x', 'xxx');";
+
+    mysqli_query($dbconn, $query);
+
+    echo "added new entry into database \n";   
+
+    $query = "INSERT INTO Products VALUES (NULL, 'Snobb glas', 1, 700000000, 1337, 'x', 'xxx');";
+
+    mysqli_query($dbconn, $query);
+
+    echo "added new entry into database \n";
+
+    $query = "INSERT INTO Products VALUES (NULL, 'Vad ska vi sälja?', 1, 1, 1, 'x', 'xxx');";
+
+    mysqli_query($dbconn, $query);
+
+    echo "added new entry into database \n";
+
+    $query = "INSERT INTO Products VALUES (NULL, 'Jeans', 670, 99, 80, 'x', 'xxx');";
+
+    mysqli_query($dbconn, $query);
+
+    echo "added new entry into database \n";
+
+
+    /* Below are entries added into the Users table */
+
+
+
+    /* Below are entries added into the Orders table */
+
+
+    
+    /* Below are entries added into the ShoppingCart table */
+
+?>
