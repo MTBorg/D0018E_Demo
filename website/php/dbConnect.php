95c62b37760ad19df0612ae8a494153c84eed7dd
Martin Terneborg <martinterneborg96@gmail.com>
2018-11-13 11:49:53 +0100

    Removed function parameters to function dbConnect and hardcoded parameters instead

--- website/php/dbConnect.php
+++ website/php/dbConnect.php
@@ -1,15 +1,16 @@
 <?php
-$username = "root";
-$servername = "localhost";
-$password = ""; #Enter your password to root user here
-$database = "database";
-
-$connection = mysqli_connect($servername, $username, $password, $database);
-
-
-if(mysqli_connect_errno()){
-	echo mysqli_connect_error();
-} else{
-	echo "Successfully connected to database";
+function dbConnect(){
+	$username = "admin";
+	$servername = "localhost";
+	$password = "adminpass";
+	$database = "maindb";
+	$connection = mysqli_connect($servername, $username, $password, $database);
+	
+	if(mysqli_connect_errno()){
+		echo mysqli_connect_error();
+	} else{
+		echo "Successfully connected to database";
+		return $connection;
+	}
 }
 ?>
