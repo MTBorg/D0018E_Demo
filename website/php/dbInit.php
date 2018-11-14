7c1b7569b291801d4de11e52438ab2fe3bd25ce7
dynematic <arostr-5@student.ltu.se>
2018-11-14 17:37:29 +0100

    products img_url are now loaded on the website

--- website/php/dbInit.php
+++ website/php/dbInit.php
@@ -1,54 +1,61 @@
 <?php
+
+    // Setup connection
     require_once 'dbConnect.php';
-
     $dbconn = dbConnect();
-
-    $query = "CREATE TABLE IF NOT EXISTS Products(
-                id INT NOT NULL AUTO_INCREMENT,
-                name VARCHAR(20) NOT NULL,
-                price INT NOT NULL CHECK (price>=0),
-                stock INT NOT NULL CHECK (stock>=0),
-                rating INT NOT NULL, 
-                comment_section VARCHAR(1),
-                img_url VARCHAR(90),
-                PRIMARY KEY(id)
-            )";
-        
-    mysqli_query($dbconn, $query);
-
-    $query = "CREATE TABLE IF NOT EXISTS Users(
-                id INT NOT NULL AUTO_INCREMENT,
-                role_id INT NOT NULL,
-                first_name VARCHAR(20) NOT NULL,
-                last_name VARCHAR(20) NOT NULL,
-                email VARCHAR(25) NOT NULL, 
-                password VARCHAR(18) NOT NULL,
-                PRIMARY KEY(id)
-            )";
-
-    mysqli_query($dbconn, $query);
-
-    $query = "CREATE TABLE IF NOT EXISTS Orders(
-                id INT NOT NULL AUTO_INCREMENT,
-                user_id INT NOT NULL,
-                product_id INT NOT NULL,
-                PRIMARY KEY(id),
-                FOREIGN KEY(user_id) REFERENCES Users(id),
-                FOREIGN KEY(product_id) REFERENCES Products(id)
-            );";
-
-    mysqli_query($dbconn, $query);
-
-    $query = "CREATE TABLE IF NOT EXISTS ShoppingCart(
-                id INT NOT NULL AUTO_INCREMENT,
-                cost_sum INT NOT NULL,
-                PRIMARY KEY(id)
-            );";
-
-    mysqli_query($dbconn, $query);
-
-    echo "The tables Products, Users, Orders and ShoppingCart created in the maindb database\n";
     
-    # Close the connection to the DB
-    mysqli_close($dbconn);
+    try {
+
+        $query = "CREATE TABLE IF NOT EXISTS Products(
+                    id INT NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(20) NOT NULL,
+                    price INT NOT NULL CHECK (price>=0),
+                    stock INT NOT NULL CHECK (stock>=0),
+                    rating INT NOT NULL, 
+                    comment_section VARCHAR(1),
+                    img_url VARCHAR(90),
+                    PRIMARY KEY(id)
+                )";
+            
+        mysqli_query($dbconn, $query);
+
+        $query = "CREATE TABLE IF NOT EXISTS Users(
+                    id INT NOT NULL AUTO_INCREMENT,
+                    role_id INT NOT NULL,
+                    first_name VARCHAR(20) NOT NULL,
+                    last_name VARCHAR(20) NOT NULL,
+                    email VARCHAR(25) NOT NULL, 
+                    password VARCHAR(18) NOT NULL,
+                    PRIMARY KEY(id)
+                )";
+
+        mysqli_query($dbconn, $query);
+
+        $query = "CREATE TABLE IF NOT EXISTS Orders(
+                    id INT NOT NULL AUTO_INCREMENT,
+                    user_id INT NOT NULL,
+                    product_id INT NOT NULL,
+                    PRIMARY KEY(id),
+                    FOREIGN KEY(user_id) REFERENCES Users(id),
+                    FOREIGN KEY(product_id) REFERENCES Products(id)
+                );";
+
+        mysqli_query($dbconn, $query);
+
+        $query = "CREATE TABLE IF NOT EXISTS ShoppingCart(
+                    id INT NOT NULL AUTO_INCREMENT,
+                    cost_sum INT NOT NULL,
+                    PRIMARY KEY(id)
+                );";
+
+        mysqli_query($dbconn, $query);
+
+        echo "The tables Products, Users, Orders and ShoppingCart created in the maindb database\n";
+    
+    
+    } catch (Exception $e) {
+        echo 'Exception: ', $e -> getMessage(), "\n";
+    }
+
+    
 ?>
