fd0a37ec3d71f7ffb08c82233fb48084095b348f
Martin Terneborg <termar-5@student.ltu.se>
2018-11-14 19:58:19 +0100

    Moved all the styles related to log in features back to their own file

--- website/php/login.php
+++ website/php/login.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="../css/LogInStyles.css" rel="stylesheet"> 
+
 	<title>Log in</title>
 </head>	
 	<nav role="navigation"> 
 
 	</nav>	
 
-	<div style="border-style:solid">
-		Hello world
+	<div class="logInBox">
+		<a href=""; class="buttonLogIn">Log In</a>
 	</div>
 
 </html>
