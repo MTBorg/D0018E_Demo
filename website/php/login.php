d1d183b10bb29152d3aefffe7dbb28d072557419
Martin Terneborg <martinterneborg96@gmail.com>
2018-11-15 13:04:34 +0100

    The login script should now correctly query the database (although there is a bug)

--- website/php/login.php
+++ website/php/login.php
@@ -1,13 +1,24 @@
 <?php
-	include_once 'dbConnect.php';
+	/*
+	There is a bug: If you put a space in the username field after entering your username
+	and then enter your password in the same field it will succeed"*/
 
+
+	include_once 'dbConnect.php';
+	
 	$email = $_POST["email"];
 	$password = $_POST["password"];	
-	
 	$connection = dbConnect();
-	$query_result = mysqli_query($connection, "SELECT * FROM Users WHERE email=".$email.";");
-	$row =  mysqli_fetch_array($query_result);
-	#echo $row['Hello world' + 'first_name'];
+	$query_result = mysqli_query($connection, 'SELECT password FROM Users WHERE email="'.$email.'";');
+	if($query_result == FALSE){
+		echo "Failed to query user";
+	}else{
+		$row = $query_result->fetch_assoc();
+		if($password == $row["password"]){
+			echo "Login success";
+		}else{
+			echo "Login failure";
+		}
+	}
 	
-	echo $email, $password; #DEBUG
 ?>
