2a2327c7a4339dd4562fec5aba5829dc0f63f47e
banunkers <hugwan-6@student.ltu.se>
2018-11-15 10:50:55 +0100

    Implemented the function orderProducts.php which return the new stock of the product after the product has been ordered

--- website/php/orderProduct.php
+++ website/php/orderProduct.php
@@ -1,15 +1,35 @@
 <?php
     
     function orderProduct($prodID) {
-        require_once 'dbConnect.php'
+        include 'dbConnect.php';
+       
         $dbconn = dbConnect();
 
-        $queryProduct = "SELECT * FROM Products WHERE Products.id == $prodID";
+        $queryProduct = "SELECT * FROM Products WHERE Products.id = $prodID";
 
         $product = mysqli_query($dbconn, $queryProduct);
         
-        
+        // If the product exists
+        if ($product) {
+            // Create an array of the query result
+            $prodInfo = mysqli_fetch_array($product);
+            
+            // The product is in stock
+            if ($prodInfo['stock'] > 0) {
+                $newStock = $prodInfo['stock'] - 1;
+                $queryOrder = "UPDATE Products SET stock=".$newStock." WHERE id=".$prodInfo['id'];
+                mysqli_query($dbconn, $queryOrder);
+
+                echo $newStock;
+            } else {
+                echo $prodInfo['stock'];
+            }
+            
+        }
 
     }
+
+    orderProduct(1);
+
 
 ?>
