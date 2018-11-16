71b3729f629bd8e9aff30d3477ee0deffdc9157f
banunkers <hugwan-6@student.ltu.se>
2018-11-16 11:15:16 +0100

    The scripts to clear and reset the tables Products and Orders are now both implemented in one file. This was because Products couldn't be cleared without Orders being cleared since it has a foreign key refrencing the product id.

--- website/php/dbResetOrdersProducts.php
+++ website/php/dbResetOrdersProducts.php
@@ -9,6 +9,12 @@
     mysqli_query($dbconn, $resetOrders);
     mysqli_query($dbconn, $resetOrdersID);
 
+    $resetProducts = "DELETE FROM Products;";
+    $resetProductsID = " ALTER TABLE Products AUTO_INCREMENT = 1;";
+
+    mysqli_query($dbconn, $resetProducts);
+    mysqli_query($dbconn, $resetProductsID);
+
     mysqli_close($dbconn);
 
 ?>
