a05ef32acbc093e5c581b43fac2643e77537420a
MTBorg <martinterneborg96@gmail.com>
2018-11-21 18:47:22 +0100

    Made the header for the shoppingcart redirect to the main page

--- website/php/shoppingCartPage.php
+++ website/php/shoppingCartPage.php
@@ -7,13 +7,14 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <header role="banner">
-        <nav role="navigation">
-
-		    <h1 id="logoText"> StarTrader </h1>
+    <header role="banner"> 
+        <a href="../index.php" style="text-decoration:none">
+            <h1 id="logoText"> StarTrader </h1>
 		    <h3 id="logoSlogan"> The biggest market in the universe </h3>
+        </a>
 
-            <i id="navIcon" class="fa fa-align-justify" style="font-size:36px; color:white"></i>
+        <nav role="navigation">
+		    <i id="navIcon" class="fa fa-align-justify" style="font-size:36px; color:white"></i>
         </nav>
     </header>
 
