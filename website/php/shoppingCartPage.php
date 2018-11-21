f2cfe75b344ca22f75167ab1b8cc5d2f6835e190
MTBorg <martinterneborg96@gmail.com>
2018-11-21 15:41:12 +0100

    Added the actual page

---
+++ website/php/shoppingCartPage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shopping Cart</title>
+
+    <link href="../css/styles.css" rel="stylesheet" media="all">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header role="banner">
+        <nav role="navigation">
+
+		    <h1 id="logoText"> StarTrader </h1>
+		    <h3 id="logoSlogan"> The biggest market in the universe </h3>
+
+            <i id="navIcon" class="fa fa-align-justify" style="font-size:36px; color:white"></i>
+        </nav>
+    </header>
+</body>
+</html>
