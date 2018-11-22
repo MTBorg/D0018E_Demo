30675b13df499aa86646fc7dddb80caac06cbd8a
TBorg <martinterneborg96@gmail.com>
2018-11-22 20:05:53 +0100

    Added a simple login page

---
+++ website/php/myOrdersPage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Orders</title>
+
+    <link rel="stylesheet" href="../css/styles.css" media="all">
+</head>
+<body>
+    <header>
+        <a href="../index.php" style = "text-decoration:none">
+		<h1 id="logoText"> StarTrader </h1>
+        <h3 id="logoSlogan"> The biggest market in the universe </h3>
+    </header>
+
+</body>
+</html>
