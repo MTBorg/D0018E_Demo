34b26ef6e2a1ab9bb687635db48da2e6849598a5
TBorg <martinterneborg96@gmail.com>
2018-11-22 20:46:02 +0100

    Added a table for the orders

---
+++ website/php/loadMyOrders.php
@@ -0,0 +1,32 @@
+<?php
+    function loadMyOrders(){
+        $user_id;
+        session_start();
+        if(isset($_SESSION["user_id"])){
+            $user_id=$_SESSION["user_id"];
+        }else{
+            echo '<p>Not logged in</p>';
+            return;
+        }
+
+        include_once 'dbConnect.php';
+        $dbConn = dbConnect(); 
+
+        if($dbConn == false){
+            echo '<p>Failed to connect to database</p>';
+            return;
+        }
+
+        $query = 'SELECT id FROM Orders WHERE user_id='.$user_id.';';
+        $orders = mysqli_query($dbConn, $query);
+        if($orders){
+            echo '<table id="myOrdersTable">';
+            echo '<tr> <th> Order ID </th> </tr>';
+            while($row = mysqli_fetch_object($orders)){
+                $order_id = $row->id;
+                echo '<tr><td><a href="">'.$order_id.'</a></td></tr>';
+            }
+            echo '</table>';
+        }
+    }
+?>
--- website/php/myOrdersPage.php
+++ website/php/myOrdersPage.php
@@ -7,10 +7,14 @@
 </head>
 <body>
     <header>
-        <a href="../index.php" style = "text-decoration:none">
+        <a href="../index.php" style = "text-decoration:none"> </a>
 		<h1 id="logoText"> StarTrader </h1>
         <h3 id="logoSlogan"> The biggest market in the universe </h3>
     </header>
-
+    
+    <?php
+        include_once 'loadMyOrders.php';
+        loadMyOrders();
+    ?>
 </body>
 </html>
