7be60318d4bb6267fa3fbf0db013c6428c048a8c
Aron Strandberg <arostr-5@student.ltu.se>
2018-11-25 14:38:27 +0100

    created php page

---
+++ website/php/productPage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="../css/styles.css" rel="stylesheet"> 
+
+	<title>Log in</title>
+	<script src="../js/LogInSubmit.js" type="text/javascript">
+	</script>
+</head>	
+<body>
+	<header role="banner">
+	<a href="../index.php" style = "text-decoration:none">
+		<h1 id="logoText"> StarTrader </h1>
+		<h3 id="logoSlogan"> The biggest market in the universe </h3>
+</a>
+
+        <nav role="navigation">
+           
+
+			<a href="createUserPage.php" id="signUpMain" class="Button">Sign up</a>
+        </nav>
+    </header>
+
+
+
+    <!-- Content -->
+	
+
+</body>
+</html>
