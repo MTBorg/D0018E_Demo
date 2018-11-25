83b986b78c899d7ad706faf95b318766ef31e3ff
TBorg <martinterneborg96@gmail.com>
2018-11-25 12:35:14 +0100

    Fixed broken tag and made urls relative to the root

--- website/php/initHeader.php
+++ website/php/initHeader.php
@@ -1,5 +1,5 @@
 <?php
-    echo 'header role="banner">
+    echo '<header role="banner">
         <a href="../index.php" style = "text-decoration:none">
 		    <h1 id="logoText"> StarTrader </h1>
             <h3 id="logoSlogan"> The biggest market in the universe </h3>
@@ -17,11 +17,11 @@
                     <a href="#">About</a>   
                 </div>
             </div>
-            <a href="../php/adminpage.php" id="adminMain" class="Button">Admin panel</a>
-	        <a href="php/createUserPage.php" id="signUpMain" class="Button initHidden">Sign up</a>
-            <a href="php/logOut.php" id="logOutMain" class="Button initHidden">Log out</a>
-            <a href="php/shoppingCartPage.php"> <i class="fa fa-shopping-cart initHidden" style="display:none" id="shoppingCart"></i></a>
-	        <a href="php/loginpage.php" id="logInMain" class="Button initHidden">Log In</a>
+            <a href="/php/adminpage.php" id="adminMain" class="Button">Admin panel</a>
+	        <a href="/php/createUserPage.php" id="signUpMain" class="Button initHidden">Sign up</a>
+            <a href="/php/logOut.php" id="logOutMain" class="Button initHidden">Log out</a>
+            <a href="/php/shoppingCartPage.php"> <i class="fa fa-shopping-cart initHidden" style="display:none" id="shoppingCart"></i></a>
+	        <a href="/php/loginpage.php" id="logInMain" class="Button initHidden">Log In</a>
         </div>
         </nav>
     </header>
