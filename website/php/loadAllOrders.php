1b30faf0570ea2b4407ede660b79fdba967a1182
TBorg <martinterneborg96@gmail.com>
2018-11-29 11:37:16 +0100

    Updated the style of the admin page

--- website/php/loadAllOrders.php
+++ website/php/loadAllOrders.php
@@ -21,7 +21,7 @@
         $orders = mysqli_query($dbConn, $query);
         if($orders){
             echo '<table class="ordersTable">';
-            echo '<tr class="ordersTableHeader"> <th> Order ID </th> <th> User ID </th> <th> Email </th> </tr>';
+            echo '<tr class="ordersTableHeader"> <th> Order ID </th> <th> User ID </th> <th> Email </th> <th> </th> </tr>';
             while($row = mysqli_fetch_object($orders)){
                 $order_id = $row->id;
                 $user_id = $row->user_id;
@@ -31,7 +31,12 @@
                 $user = mysqli_fetch_object($query_user);
                 $user_email = $user->email;
                 
-                echo '<tr><td><a href="adminOrderPage.php?order_id='.$order_id.'">'.$order_id.'</a></td> <td>'.$user_id.'</td> <td>'.$user_email.'</td></tr>';
+                echo '<tr>
+                        <td>'.$order_id.' </td>
+                        <td>'.$user_id.' </td>
+                        <td>'.$user_email.'</td>
+                        <td><button  onclick=\'location.href="adminOrderPage.php?order_id='.$order_id.'"\'>View </td>
+                        </tr>';
                 
             }
             echo '</table>';
