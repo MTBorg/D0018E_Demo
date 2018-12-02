b98afb9fd142bd028991098ae9f27c5e5250c1cd
Aron Strandberg <arostr-5@student.ltu.se>
2018-12-02 21:35:59 +0100

    each users rating now also shows in amount of stars they rated

--- website/js/initReviews.js
+++ website/js/initReviews.js
@@ -6,30 +6,40 @@ function initReviews() {
 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
       if (this.responseText) {
         var reviewData = this.responseText.split("|");
         console.log(reviewData);
-        for (var i = 0; i < reviewData.length - 1; i += 2) {
-          document
-            .getElementById("userBox")
-            .appendChild(document.createElement("div")).innerHTML =
-            reviewData[i + 1];
-          document
-            .getElementById("userBox")
-            .appendChild(document.createElement("textarea")).innerHTML =
-            reviewData[i];
-            document
-            .getElementById("userBox").getElementsByTagName("textarea")[i].disabled = true;
-        }
-
-        // Random colors for user box
-        for (var i = 0; i < reviewData.length / 2 - 1; i++) {
+        for (var i = 0; i < reviewData.length - 1; i += 3) {
+          // Random colors for user box
           //Ref: https://css-tricks.com/snippets/javascript/random-hex-color/
           var randomNr =
             "#" + Math.floor(Math.random() * 16777215).toString(16);
-          document.getElementById("userBox").getElementsByTagName("div")[
-            i
-          ].style.backgroundColor = randomNr;
+
+          var userBox = document.getElementById("userBox");
+          var nameAndRatingBox = document.createElement("div");
+          
+          var commentBox = document.createElement("textarea");
+          
+          // Install div where name and rating shows
+          userBox.appendChild(nameAndRatingBox);
+          // array ["comment", "rating", "user"]
+          nameAndRatingBox.innerHTML = reviewData[i + 2];
+          nameAndRatingBox.style.backgroundColor = randomNr;
+
+          // Install stars dependent on user rating 1-5
+          for(var a = 0; a < reviewData[i + 1]; a++) {
+            var starSpan = document.createElement("span");
+            starSpan.className = "fa fa-star";
+            userBox.appendChild(starSpan);
+
+        }
+
+          // Install textarea where comment shows
+          userBox.appendChild(commentBox);
+
+          commentBox.innerHTML = reviewData[i];
+          commentBox.disabled = true;
         }
       }
     }
