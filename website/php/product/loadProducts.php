7d9f3b25de58b9a6aa8e2569ef2a1347221c7e52
Aron Strandberg <arostr-5@student.ltu.se>
2018-12-05 17:38:20 +0100

    ontrack to solve product div clickable

--- website/php/product/loadProducts.php
+++ website/php/product/loadProducts.php
@@ -19,23 +19,34 @@
         if($products) {
             // Fetch associated array
             while ($row = mysqli_fetch_array($products)) {
-                // Hence we do not need to know the url only the name
-                echo '<div id="'.$row['id'].'" class="item">';
-                echo '<img src="'. $row['img_url'] .'">';
-                echo '<p id="namePos">' . $row['name'] . '</p>';
-                echo '<p id="pricePos">' . $row['price'] . '$</p>';
-                echo '<p id="stockPos"> <b>stock</b>: ' . $row['stock'] . '</p>';
-                // echo '<p id="ratingPos"> <b>rating</b>: ' . $row['rating'] . '</p>';
-
-                if(isAdmin()) {
-                    echo '<a href="/php/pages/adminProductPage.php?product_id='.$row['id'].'"><button>Reviews</button></a>';
-                    echo '<button id="'.$row['id'].'" class="addToCartButton" type="button">Add to cart <i class="fa fa-shopping-cart"></i></button>';
-                } else {
-                    echo '<a href="/php/pages/productPage.php?product_id='.$row['id'].'"><button>Reviews</button></a>';
-                    echo '<button id="'.$row['id'].'" class="addToCartButton" type="button" onclick="addToCartOnClick(this.id)">Add to Cart<i class="fa fa-shopping-cart"></i></button>';
-                }
-                echo '</div>';
                 
+                
+                // This is really stupid...
+                if(isAdmin()) {
+                    echo '<div id="'.$row['id'].'" class="item">';
+                    // echo '<button id="'.$row['id'].'" class="addToCartButton" type="button">Add to cart <i class="fa fa-shopping-cart"></i></button>';
+                    echo '<img src="'. $row['img_url'] .'">';
+                    echo '<p id="namePos">' . $row['name'] . '</p>';
+                    echo '<p id="pricePos">' . $row['price'] . '$</p>';
+                    echo '<p id="stockPos"> <b>stock</b>: ' . $row['stock'] . '</p>';
+                    // echo '<p id="ratingPos"> <b>rating</b>: ' . $row['rating'] . '</p>';
+                    echo '<p><a class="div-click" href="/php/pages/adminProductPage.php?product_id='.$row['id'].'"></a></p>';
+
+
+                } else {
+                    echo '<div id="'.$row['id'].'" class="item">';
+                    echo '<img src="'. $row['img_url'] .'">';
+                    echo '<p id="namePos">' . $row['name'] . '</p>';
+                    echo '<p id="pricePos">' . $row['price'] . '$</p>';
+                    echo '<p id="stockPos"> <b>stock</b>: ' . $row['stock'] . '</p>';
+                    // echo '<p id="ratingPos"> <b>rating</b>: ' . $row['rating'] . '</p>';
+                    echo '<button id="'.$row['id'].'" class="addToCartButton" type="button" onclick="addToCartOnClick(this.id)">Add to Cart<i class="fa fa-shopping-cart"></i></button>';
+                    echo '<p><a class="div-click" href="/php/pages/productPage.php?product_id='.$row['id'].'"></a></p>';
+
+                }
+
+                echo '</div>';
+
             }
          } else {
             echo '<p> No products ... </p>';
