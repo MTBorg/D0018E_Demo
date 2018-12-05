2d70630d6db2dc976df26030dc4006833e84fc66
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-05 21:20:47 +0100

    The my orders page now displays the order status

--- website/php/order/loadMyOrders.php
+++ website/php/order/loadMyOrders.php
@@ -17,16 +17,20 @@
             return;
         }
 
-        $query = 'SELECT id FROM Orders WHERE user_id='.$user_id.';';
+        $query = 'SELECT id, status FROM Orders WHERE user_id='.$user_id.';';
         $orders = mysqli_query($dbConn, $query);
         if($orders){
             echo '<table class="ordersTable">';
-            echo '<tr class="ordersTableHeader"> <th> Order ID </th> </tr>';
+            echo '<tr class="ordersTableHeader"> <th> Order ID </th> <th> Status </th> </tr>';
             while($row = mysqli_fetch_object($orders)){
                 $order_id = $row->id;
+                $order_status = $row->status;
 
                 $href = '\'location.href="/php/pages/orderPage.php?order_id='.$order_id.'"\'';
-                echo '<tr><td><button style="padding-right:10px;padding-left:10px" type="button" onclick='.$href.'>'.$order_id.'</button></td></tr>';
+                echo '<tr>
+                        <td><button style="padding-right:10px;padding-left:10px" type="button" onclick='.$href.'>'.$order_id.'</button></td>
+                        <td>'.$order_status.'</td>
+                      </tr>';
             }
             echo '</table>';
         }
