12a0d23b2ddf80ab4b782ab0aa87e8ff5e6a46d8
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-05 14:58:09 +0100

    The mechanic should now works as expected using a better code structure

--- website/js/addToCartOnClick.js
+++ website/js/addToCartOnClick.js
@@ -12,14 +12,13 @@ function addToCartOnClick(product_id) {
       }else if(this.responseText.replace(/\s/g,"") != ""){ //This is necessary to check for empty whitespaces
         alert(this.responseText);
       }else{
-        
+        shopCartBtnUpdateQuantitySubmit();
       } 
     }
 
   };
 
   // addToCartOnClick, if user is logged in ok
-  //TODO: Using a synchronous call gives a warning, but it's necessary to make the shopping cart button quantity functionality to work, how to fix?
-  xmlhttp.open("GET", "/php/cart/addToCart.php?id=" + product_id, false); 
+  xmlhttp.open("GET", "/php/cart/addToCart.php?id=" + product_id, true); 
   xmlhttp.send();
 }
