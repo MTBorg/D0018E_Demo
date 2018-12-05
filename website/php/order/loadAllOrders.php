c6496144da31f290883a331939af3561455b8ce2
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-05 19:01:20 +0100

    The admin page should now display the orders status

--- website/php/order/loadAllOrders.php
+++ website/php/order/loadAllOrders.php
@@ -21,10 +21,11 @@
         $orders = mysqli_query($dbConn, $query);
         if($orders){
             echo '<table class="ordersTable">';
-            echo '<tr class="ordersTableHeader"> <th> Order ID </th> <th> User ID </th> <th> Email </th> <th> </th> </tr>';
+            echo '<tr class="ordersTableHeader"> <th> Order ID </th> <th> User ID </th> <th> Email </th> <th> Status </th> <th> </th> </tr>';
             while($row = mysqli_fetch_object($orders)){
                 $order_id = $row->id;
                 $user_id = $row->user_id;
+                $order_status = $row->order_status;
                 
                 # Get the users email
                 $query_user = mysqli_query($dbConn, 'SELECT email FROM Users WHERE id = '.$user_id);
@@ -35,6 +36,7 @@
                         <td>'.$order_id.' </td>
                         <td>'.$user_id.' </td>
                         <td>'.$user_email.'</td>
+                        <td>'.$order_status.'</td>
                         <td><button  onclick=\'location.href="/php/pages/adminOrderPage.php?order_id='.$order_id.'"\'>View </button></td>
                         </tr>';
                 
