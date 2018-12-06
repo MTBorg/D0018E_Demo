c910a39cdaa4dd6d974dca8d1fc1571eec50f93d
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-06 20:23:10 +0100

    The shopping cart button now automatically updates

--- website/js/cart/shopCartAlterQuantitySubmit.js
+++ website/js/cart/shopCartAlterQuantitySubmit.js
@@ -6,8 +6,12 @@ function shopCartAlterQuantitySubmit(user_id, product_id, increase, product_pric
 			if (this.response == true){
 				//Set new quantity
 				let element = document.getElementById("quantity"+product_id);
-				let newQuantity = (parseInt(element.innerText) + (increase == 1 ? 1 : -1)).toString();
-				element.innerText = newQuantity;
+				let newQuantity = parseInt(element.innerText) + (increase == 1 ? 1 : -1);
+				if(newQuantity <= 0){
+					removeShoppingCartItemSubmit(user_id, product_id);
+					return; //To prevent the user from seeing the quantity turn to 0
+				}
+				element.innerText = newQuantity.toString();
 				
 				//Set new sum
 				document.getElementById("sum"+product_id).innerText = (parseInt(newQuantity) * product_price).toString();
@@ -15,6 +19,12 @@ function shopCartAlterQuantitySubmit(user_id, product_id, increase, product_pric
 				//Set the new total sum
 				let element_sumtot = document.getElementById("totalSum");
 				element_sumtot.innerText = (parseInt(element_sumtot.innerText) + product_price * (increase == 1 ? 1 : -1)).toString();
+
+				//Set the shopping cart button
+				let element_cartquantity = document.getElementById("shoppingCartBtnQuantity");
+				let cartquantity = element_cartquantity.innerText.substr(1, element_cartquantity.innerText.length); //Remove the parenthesis around the number
+				let cartquantity_new = parseInt(cartquantity) + (increase == 1 ? 1 : -1);
+				element_cartquantity.innerText = "(" + cartquantity_new.toString() + ")";
 			}else{
 				alert(this.responseText);
 			}
