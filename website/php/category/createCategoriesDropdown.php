484059b60e4324e1764d792fc988b7484541ab83
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-09 19:50:49 +0100

    Implemented small arrows when hovering categories

--- website/php/category/createCategoriesDropdown.php
+++ website/php/category/createCategoriesDropdown.php
@@ -10,7 +10,7 @@
     $categories = mysqli_query($dbConn, 'SELECT * FROM Categories;');
     if($categories){
         while($cat = mysqli_fetch_object($categories)){
-            $result = $result. '<a href="/php/pages/categoryPage.php?cat_id='.$cat->id.'">'.$cat->cat_name.'</a>';
+            $result = $result. '<a href="/php/pages/categoryPage.php?cat_id='.$cat->id.'">'.$cat->cat_name.'<span> </span></a>';
         }
     }else{
         return 'Failed to retrieve categories: '.mysqli_error($dbConn);
