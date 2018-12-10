3a2929d691c4f7fabfc06dc7edfc61c490411cf7
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-10 20:17:44 +0100

    Made the test script generating random customer generate random emails for the customers

--- website/php/test/spamCustomers.php
+++ website/php/test/spamCustomers.php
@@ -13,14 +13,27 @@
         return;
     }
 
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY'; //String holding all characters possible to generate
+    $email_length_max = 15;
+    
     $customer_amount = $_GET["amount"];
     $customers_inserted = $customer_amount;
     for($i = 0; $i < $customer_amount; $i++){
+
+        //Generate random email
+        $email_length = rand(1, $email_length_max);
+        $email = '"';
+        for($j = 0; $j < $email_length; $j++){
+            $email = $email.$chars[rand(0, strlen($chars) - 1)];
+        }
+        $email = $email.'.com"';
+
+
         $query = 'INSERT INTO Users VALUES(NULL, 
                                                 0,' //role_id (customer=0)
                                                 .$i.',' //first name
                                                 .$i.',' //last name
-                                                .$i.',' //email
+                                                .$email.',' //email
                                                 .$i.');'; //password
         if(!mysqli_query($dbConn, $query)){
             echo "<p>Failed to insert customer</p>";
