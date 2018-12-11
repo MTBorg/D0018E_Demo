d1560cb8e2323d5ea59e30cf0af9c9e60f9c37cf
Aron Strandberg <arostr-5@student.ltu.se>
2018-12-11 20:59:23 +0100

    removed normalize from adminproductpage

--- website/php/pages/adminProductPage.php
+++ website/php/pages/adminProductPage.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="/css/normalize.css" rel="stylesheet" media="all">
 	<link href="/css/styles.css" rel="stylesheet"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
