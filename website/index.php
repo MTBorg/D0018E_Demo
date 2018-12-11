b1d17efaf47ec8dd905f528cbabafecb87676f0f
Aron Strandberg <arostr-5@student.ltu.se>
2018-12-11 21:04:23 +0100

    finally removed normalize from index.php

--- website/index.php
+++ website/index.php
@@ -3,29 +3,17 @@
 
 <head>
     <meta charset="utf-8">
-
     <title>StarTrader</title>
-
     <!-- Enter a brief description of your page -->
     <meta name="a e-commerce website with relation database" content="not yet decided">
-
     <!-- Define a viewport to mobile devices to use - telling the browser to assume that the page is as wide as the device (width=device-width,
     and setting the initial page zoom level to be 1 (initial-scale=1.0), and fixing width=device-width for iOS9 (shtink-to-fit=no) -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-
-    <!-- Add normalize.css which enables browsers to render all elements more consistently and in line with modern standards as it only targets particular styles that need normalizing -->
-    <link href="/css/normalize.css" rel="stylesheet" media="all">
-    <!-- For legacy support (IE 6/7, Firefox < 4, and Safari < 5) use normalize-legacy.css instead -->
     <link rel="shortcut icon" href="fa-rocket.ico">
-    <!--<link href="css/normalize-legacy.css" rel="stylesheet" media="all">-->
-
     <!-- Include the site stylesheet -->
     <link href="/css/styles.css" rel="stylesheet" media="all">
-
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
-    <!-- Include the HTML5 shiv print polyfill for Internet Explorer browsers 8 and below -->
-    <!--[if lt IE 9]><script src="js/html5shiv-printshiv.js" media="all"></script><![endif]-->
     <script src="/js/cart/addToCartOnClick.js" type="text/javascript"></script>
     <script src="/js/init/initNavButtons.js" type="text/javascript"></script> 
     <script src="/js/cart/shopCartBtnUpdateQuantitySubmit.js" type="text/javascript"></script>
