1f6ed4987270a1cec34961640c67e122f31cc201
Martin Terneborg <martinterneborg96@gmail.com>
2018-12-11 20:19:34 +0100

    Fixed the missing shopping cart icon

--- website/php/pages/shoppingCartPage.php
+++ website/php/pages/shoppingCartPage.php
@@ -9,8 +9,8 @@
     <title>Shopping Cart</title>
     <link rel="shortcut icon" href="/fa-rocket.ico">
     <link href="/css/styles.css" rel="stylesheet" media="all">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.5.0/css/all.css' integrity='sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU' crossorigin='anonymous'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="/js/cart/checkoutSubmit.js" type="text/javascript"></script>
     <script src="/js/init/initNavButtons.js" type="text/javascript"></script>
     <script src="/js/cart/shopCartAlterQuantitySubmit.js" type="text/javascript"></script>
